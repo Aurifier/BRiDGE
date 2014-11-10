ca386aa578357d05dfb099fd92479f220647d14e
fizzleburger <isaacfcollege@gmail.com>
2014-11-09 23:13:00 -0500

    Fleshed out ViewContact class

--- app/src/main/java/com/caffeinecraft/bridge/ContactList.java
+++ app/src/main/java/com/caffeinecraft/bridge/ContactList.java
@@ -3,20 +3,19 @@ package com.caffeinecraft.bridge;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import com.caffeinecraft.bridge.model.Contact;
 import java.util.ArrayList;
 import java.util.List;
-
+import com.caffeinecraft.bridge.model.Contact;
 import com.caffeinecraft.bridge.dao.ContactsDataSource;
 
 public class ContactList extends Activity implements View.OnClickListener{
 
-    private ListView lv;
-    List<Contact> contactlist;
+    List<Contact> allcontacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +23,28 @@ public class ContactList extends Activity implements View.OnClickListener{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
 
-        lv = (ListView) findViewById(R.id.listView);
+        ListView lv = (ListView) findViewById(R.id.listView);
 
         ContactsDataSource foo = new ContactsDataSource(this);
         foo.open();
-        contactlist = foo.getAllContacts();
+        allcontacts = foo.getAllContacts();
 
         List<String> nameList = new ArrayList<String>();
 
-        for(int i = 0;i<contactlist.size();i++)
+        //Dummy data, until we can get real contacts.
+        for(int i = 0; i< 10; i++)
         {
-            Contact temp = contactlist.get(i);
-            nameList.add(temp.getFirstName() + " " + temp.getLastName());
+            Contact temp = new Contact();
+            temp.setFirstName("Player");
+            temp.setLastName("Number " + i);
+            temp.setId( (long) i);
+            allcontacts.add(temp);
         }
 
-        //Dummy data, until we can get real contacts.
-        for(int i = 0; i< 20; i++)
+        for(int i = 0;i<allcontacts.size();i++)
         {
-            nameList.add("Player " + i);
+            Contact temp = allcontacts.get(i);
+            nameList.add(temp.getFirstName() + " " + temp.getLastName());
         }
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
@@ -51,19 +54,19 @@ public class ContactList extends Activity implements View.OnClickListener{
 
         lv.setAdapter(arrayAdapter);
 
+        for(int i = 0; i< allcontacts.size(); i++)
+        {
+            Log.d("BRiDGE", "CONTACT " + i + ", NAMED " + allcontacts.get(i).getLastName() + "'S ID IS " + allcontacts.get(i).getId());
+        }
+
         lv.setOnItemClickListener(new ListView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> a, View v, int i, long l) {
-                //Create the intent
                 Intent intent = new Intent(getApplicationContext(), ViewContact.class);
-
-                //Create the bundle
                 Bundle bundle = new Bundle();
-                //Add your data to bundle
-                bundle.putString("row number",""+l);
-                //Add the bundle to the intent
+                long id = allcontacts.get( (int) l).getId();
+                Log.d("BRiDGE", "ACTUAL CONTACTID IS " + id);
+                bundle.putLong("contactid",id);
                 intent.putExtras(bundle);
-
-                //Fire that second activity
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/caffeinecraft/bridge/ViewContact.java
+++ app/src/main/java/com/caffeinecraft/bridge/ViewContact.java
@@ -4,7 +4,15 @@ package com.caffeinecraft.bridge;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import com.caffeinecraft.bridge.dao.ContactsDataSource;
+import com.caffeinecraft.bridge.model.Contact;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewContact extends Activity implements View.OnClickListener{
 
@@ -12,10 +20,38 @@ public class ViewContact extends Activity implements View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact_list);
+        setContentView(R.layout.activity_view_contact);
+
+        ContactsDataSource foo = new ContactsDataSource(this);
+        foo.open();
 
         Bundle bundle = getIntent().getExtras();
-        String stuff = bundle.getString("row number");
+        Long contactid = bundle.getLong("contactid");
+        Log.d("BRiDGE", "ACTUAL CONTACTID IS " + contactid);
+
+        List<Contact> allcontacts = foo.getAllContacts();
+        List<Contact> thiscontact = new ArrayList<Contact>();
+
+        Log.d("BRiDGE", "POINT ONE");
+
+        for(int i = 0;i<allcontacts.size();i++)
+        {
+            if(allcontacts.get(i).getId()==contactid) thiscontact.add(allcontacts.get(i));
+        }
+
+        TextView txtName=(TextView)findViewById(R.id.textName);
+
+        Log.d("BRiDGE", "POINT TWO");
+
+        Log.d("BRiDGE", "ACTUAL CONTACTID IS " + contactid);
+        for(int i = 0; i< allcontacts.size(); i++)
+        {
+            Log.d("BRiDGE", "CONTACT " + i + ", NAMED " + allcontacts.get(i).getLastName() + "'S ID IS " + allcontacts.get(i).getId());
+        }
+
+        txtName.setText(thiscontact.get(0).getFirstName()+" "+thiscontact.get(0).getLastName());
+
+        Log.d("BRiDGE", "POINT THREE");
 
         View btnConversationList = findViewById(R.id.buttonConversationList);
         btnConversationList.setOnClickListener(this);
@@ -30,7 +66,7 @@ public class ViewContact extends Activity implements View.OnClickListener{
                 startActivity(new Intent(this, ImportContacts.class));
                 break;
             case R.id.buttonContactList:
-                //You're already in ContactList, silly
+                startActivity(new Intent(this, ContactList.class));
                 break;
         }
     }
