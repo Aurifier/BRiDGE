2cf723cfc3c9160408fdbd4b6e14ba13fdf74cb5
fizzleburger <isaacfcollege@gmail.com>
2014-11-12 09:38:21 -0500

    Merge Contact Now Fully Functional

--- app/src/main/java/com/caffeinecraft/bridge/MergeContact.java
+++ app/src/main/java/com/caffeinecraft/bridge/MergeContact.java
@@ -1,11 +1,16 @@
 package com.caffeinecraft.bridge;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -49,13 +54,15 @@ public class MergeContact extends Activity implements View.OnClickListener{
         txtName.setText(thiscontact.toString());
 
         //Set up Listview
-        lv = (ListView) findViewById(R.id.listView);
         List<String> nameList = new ArrayList<String>();
         for(int i = 0;i<allcontacts.size();i++)
         {
             Contact temp = allcontacts.get(i);
             nameList.add(temp.toString());
         }
+
+        lv = (ListView) findViewById(R.id.listView);
+        lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 this,
                 android.R.layout.simple_list_item_multiple_choice,
@@ -87,6 +94,10 @@ public class MergeContact extends Activity implements View.OnClickListener{
                 SparseBooleanArray checked = lv.getCheckedItemPositions();
                 for (int i = 0; i < len; i++)
                     if (checked.get(i)) {
+                        Log.v("bridge", "" + i);
+                    }
+                for (int i = 0; i < len; i++)
+                    if (checked.get(i)) {
                         List<ContactMethod> checkedContactsContactMethods = allcontacts.get(i).getContactMethods();
                         for(ContactMethod contactMethod : checkedContactsContactMethods)
                         {
@@ -99,4 +110,54 @@ public class MergeContact extends Activity implements View.OnClickListener{
                 break;
         }
     }
+
+    /*
+    private class MyCustomAdapter extends ArrayAdapter<String> {
+
+        private ArrayList<String> nameList;
+
+        public MyCustomAdapter(Context context, int textViewResourceId,
+                               ArrayList<String> nl) {
+            super(context, textViewResourceId, nl);
+            this.nameList = new ArrayList<String>();
+            this.nameList.addAll(nl);
+        }
+
+        private class ViewHolder {
+            TextView code;
+            CheckBox name;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+            Log.v("ConvertView", String.valueOf(position));
+
+            if (convertView == null) {
+                LayoutInflater vi = (LayoutInflater) getSystemService(
+                        Context.LAYOUT_INFLATER_SERVICE);
+                convertView = vi.inflate(R.layout.contact_with_checkbox, null);
+
+                holder = new ViewHolder();
+                holder.code = (TextView) convertView.findViewById(R.id.code);
+                holder.name = (CheckBox) convertView.findViewById(R.id.checkBox1);
+                convertView.setTag(holder);
+
+                holder.name.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        CheckBox cb = (CheckBox) v;
+                        //stuff to do
+                    }
+                });
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            Contact contact = allcontacts.get(position);
+            holder.name.setTag(contact);
+
+            return convertView;
+        }
+    }*/
 }
